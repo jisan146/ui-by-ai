cc2ebd42b6f2c2ea1daf2946b73f245263f2dda5
Tareq Rahman Jisan <jisan146@gmail.com>
2026-08-01 12:54:47 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import "./assets/vendor/bootstrap/css/bootstrap.min.css" 
-import "./assets/vendor/bootstrap-icons/bootstrap-icons.css"
+
+import "./assets/vendor/bootstrap/css/bootstrap.min.css";
+import "./assets/vendor/bootstrap-icons/bootstrap-icons.css";
 
 import "./App.css";
 
 import Sidebar from "./components/Sidebar/Sidebar";
+
 
 class App extends Component {
   render() {
@@ -15,7 +17,9 @@ class App extends Component {
 
         <main className="app-content">
 
-          {/* Header এখানে বসবে */}
+     {/* Header এখানে পরে বসবে */}
+
+         
 
           <div className="container-fluid py-5">
             <h3 className="fw-bold text-dark">
@@ -23,7 +27,7 @@ class App extends Component {
             </h3>
           </div>
 
-          {/* Footer এখানে বসবে */}
+          {/* Footer এখানে পরে বসবে */}
 
         </main>
 
